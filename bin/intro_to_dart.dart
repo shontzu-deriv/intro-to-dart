6013c3706230b615d6cc281b21b543eaefc56b62
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-07-26 12:36:41 +0800

    git init

---
+++ bin/intro_to_dart.dart
@@ -0,0 +1,22 @@
+/// display some biodata
+import 'package:intro_to_dart/intro_to_dart.dart' as intro_to_dart;
+
+void main(List<String> arguments) {
+String name ='yuuchin';
+int age = 22;
+double height = 153.00;
+double weight = 46.5;
+bool something = true; 
+
+/* 
+display biodata 
+*/
+print(name);
+print(age);
+print(height);
+print(weight);
+print(something);
+
+print('my name is $name and I am $age year old');
+
+}
