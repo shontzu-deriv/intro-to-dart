709afdcdaecffb5f5a1576ffaf6d648f543e8c66
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-07-27 17:07:18 +0800

    todo:REVISE

--- bin/futures.dart
+++ bin/futures.dart
@@ -2,48 +2,53 @@ import 'dart:math';
 
 void main() async {
 //async await
-  double price =  await getPrice();
-  showPrices(double.parse(price.toStringAsFixed(2)));
+//   double price =  await getPrice();
+//   showPrices(double.parse(price.toStringAsFixed(2)));
 
 //promise
-  getPrice().then((val) {
-    showPrices(double.parse(val.toStringAsFixed(2)));
-  });
+//   getPrice(randomTime()).then((val) {
+//     showPrices(double.parse(val.toStringAsFixed(2)));
+//   });
 
-//stream
-  Stream<double> stream = getPricesStream();
+//stream (decides in advance what to do with a future piece of data while dart event loop continues)
+  Stream<Details> stream = getPricesStream();
   stream.listen((data) {
-    showPrices(double.parse(data.toStringAsFixed(2)));
+    showPrices(double.parse(data.price.toStringAsFixed(2)),data.time);
   });
-
 }
 
+class Details {
+  int time;
+  double price;
+  Details(int this.time, double this.price);
+}
 
 
 //stream get a stream of random prices
-Stream<double> getPricesStream() async* {
+Stream<Details> getPricesStream() async* {
   final int numberOfResults = 5;
   for (var i = 0; i < numberOfResults; i++) {
-    final price = await getPrice();
-    yield price;
+    final time = randomTime();
+    final price = await getPrice(time);
+    yield Details(time, price);
   }
 }
 
-
-Future<double> getPrice() {
+Future<double> getPrice(int time) {
   final randomPrice = Random();
   //final randomTime = Random().nextInt(5) + 1;
   return Future.delayed(
-    Duration(seconds: randomTime()),
+    Duration(seconds: time),
     () => randomPrice.nextDouble() * 1000,
   );
 }
 
-void showPrices(double price) {
-  print(price);
+void showPrices(double price,int time) {
+  print('price: $price     time:$time');
 }
 
 int randomTime() {
-  return Random().nextInt(5) + 1;
+  int time = Random().nextInt(5) + 1;
+  return time;
 }
 
